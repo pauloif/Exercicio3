4933e33e943c6977188bbc02e7ee2fca3e5f8d50
pauloif <pauloigor48@gmail.com>
2023-04-07 21:06:03 -0300

    atualizando index

--- components/menu.php
+++ components/menu.php
@@ -8,7 +8,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         
                         <!-- Nav Cadastro -->
                         <li class="nav-item dropdown">
